ed3ceb2c4b933cd9165b886663503c60bf963140
notabn <natalia.balcu@gmail.com>
2017-05-02 08:31:06 +0200

    add script to calibrate camera

---
+++ calibrate_camera.py
@@ -0,0 +1,80 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import glob
+import pickle
+
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+
+
+#number of inside corners in x
+nx = 9
+#number of inside corners in y
+ny = 6
+
+objp = np.zeros((nx*ny,3), np.float32)
+objp[:,:2] = np.mgrid[0:nx, 0:ny].T.reshape(-1,2)
+
+
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d points in real world space
+imgpoints = [] # 2d points in image plane.
+
+
+# Make a list of calibration images
+images = glob.glob('camera_cal/calibration*.jpg')
+
+def cal_undistort(img, objpoints, imgpoints):
+    # Use cv2.calibrateCamera() and cv2.undistort()
+    gray = cv2. cvtColor(img, cv2.COLOR_BGR2GRAY)
+    undist = cv2.undistort(img, mtx, dist, None, mtx)
+
+    return undist
+
+
+
+for idx, fname in enumerate(images):
+    img = cv2.imread(fname)
+    # Convert to grayscale
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # Find the chessboard corners
+    ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
+    # If found, draw corners
+    if ret == True:
+        # Draw and display the corners
+        imgpoints.append(corners)
+        objpoints.append(objp)
+        img = cv2.drawChessboardCorners(img, (nx, ny), corners, ret)
+        write_name = 'corners_found'+str(idx)+'.jpg'
+        cv2.imwrite(write_name, img)
+        cv2.imshow('img', img)
+        cv2.waitKey(500)
+
+cv2.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+# Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
+dist_pickle = {}
+dist_pickle["mtx"] = mtx
+dist_pickle["dist"] = dist
+pickle.dump(dist_pickle, open("wide_dist_pickle.p", "wb"))
+
+# Test undistortion on an image
+img = cv2.imread('camera_cal/calibration1.jpg')
+dst = cal_undistort(img, objpoints, imgpoints)
+cv2.imwrite('test_undist.jpg',dst)
+
+
+#dst = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
+# Visualize undistortion
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(15,10))
+ax1.imshow(img)
+ax1.set_title('Original Image', fontsize=30)
+ax2.imshow(dst)
+ax2.set_title('Undistorted Image', fontsize=30)
+plt.show()
+print('done')
+
